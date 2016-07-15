3a3d2e01336e2fd73cf808b9aa74a765aec4ea58
godliness <1229727237@qq.com>
2016-07-15 14:21:25 +0800

    module

--- app/src/main/java/com/disklrucache/android/diskcache/cachemanager/DiskCacheManager.java
+++ app/src/main/java/com/disklrucache/android/diskcache/cachemanager/DiskCacheManager.java
@@ -477,7 +477,7 @@ public class DiskCacheManager extends BaseCacheManager {
      * @param key
      * @param value
      */
-    public void put(String key, LS value) {
+    public void put(String key, Serializable value) {
         if (TextUtils.isEmpty(key) || value == null) return;
         DiskLruCache.Editor editor = null;
         ObjectOutputStream oos = null;
